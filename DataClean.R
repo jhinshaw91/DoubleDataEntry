69c7122c9a4fb9218bac6c5bb3ecd69142d7a0cb
“jhinshaw91” <jessica.hinshaw@amoshealth.org>
2020-03-14 11:21:45 -0600

    jh

--- DataClean.R
+++ DataClean.R
@@ -2,15 +2,31 @@ install.packages("dataCompareR")
 library('dataCompareR')
 library(dplyr)
 library(tidyr)
-class(Set_1)
-class(Set_2)
+library(knitr)
+library(stringi)
+library(markdown)
+library(devtools)
+install_git('https://github.com/capitalone/dataCompareR.git', branch = 'master',
+            subdir = 'dataCompareR', type = 'source', repos = NULL,
+            build_vignettes = TRUE)
 
 Set1<-data.frame(lapply(Set_1, as.character), stringsAsFactors=FALSE)
 Set2<-data.frame(lapply(Set_2, as.character), stringsAsFactors=FALSE)
-coerce(Set1,Set2)
-comparison<-dataCompareR::rCompare(Set1,Set2, keys = 'record_id')
-summary(comparison)
-print(comparison)
 
-generateMismatchData(comparison,Set_1,Set_2)
+compSets <- rCompare(Set1, Set2, keys = 'record_id')
+
+summary(compSets)
+
+print(compSets)
+
+mismatches <- generateMismatchData(compSets, Set1, Set2)
+
+mismatches
+
+saveReport(compSets, reportName = 'testReport')
+
+
+
+
+
 
